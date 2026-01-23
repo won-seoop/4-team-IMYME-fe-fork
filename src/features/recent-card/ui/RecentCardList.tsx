4820a656b79ea563d1452b0586656f5110358a80
오수진 <osujin@osujin-ui-MacBookPro.local>
2026-01-23 09:04:50 +0900

    feat: recent card list ui

---
+++ src/features/recent-card/ui/RecentCardList.tsx
@@ -0,0 +1,10 @@
+import { RecentCard } from '@/shared'
+
+export function RecentCardList() {
+  return (
+    <div className="mt-5 flex flex-col items-center gap-4">
+      <RecentCard />
+      <RecentCard />
+    </div>
+  )
+}
