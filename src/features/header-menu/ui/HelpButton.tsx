86f5012cab4051033e04e7e6ad3e501d75eca0ec
오수진 <osujin@osujin-ui-MacBookPro.local>
2026-02-04 13:48:51 +0900

    fix: click help button

--- src/features/header-menu/ui/HelpButton.tsx
+++ src/features/header-menu/ui/HelpButton.tsx
@@ -1,5 +1,17 @@
+import { toast } from 'sonner'
+
 import { Button } from '@/shared/ui/button'
 
 export function HelpButton() {
-  return <Button variant={'modal_btn_primary'}>도움말</Button>
+  const handleHelp = () => {
+    toast.info('준비 중입니다!')
+  }
+  return (
+    <Button
+      variant={'modal_btn_primary'}
+      onClick={handleHelp}
+    >
+      도움말
+    </Button>
+  )
 }
